bb8122c720c4a1cfa208d74ee054ce4cd1514255
田一帆 <yifan.tian@tongdun.net>
2022-07-06 10:23:18 +0800

    用户登陆注册，整合redis

--- eatmeat-api/src/main/java/com/xiaotian/controller/PassportController.java
+++ eatmeat-api/src/main/java/com/xiaotian/controller/PassportController.java
@@ -1,20 +1,22 @@
 package com.xiaotian.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.xiaotian.pojo.User;
+import com.xiaotian.pojo.bo.ShopCatBO;
 import com.xiaotian.pojo.bo.UserBO;
 import com.xiaotian.service.UserService;
-import com.xiaotian.utils.CookieUtils;
-import com.xiaotian.utils.JsonUtils;
-import com.xiaotian.utils.MD5Utils;
-import com.xiaotian.utils.Response;
+import com.xiaotian.utils.*;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Api(value = "注册登录", tags = {"用于注册登录的相关接口"})
 @RestController
@@ -23,6 +25,9 @@ public class PassportController extends BaseController{
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RedisOperator redisOperator;
 
     @ApiOperation(value = "用户名是否存在", notes = "用户名是否存在", httpMethod = "GET")
     @GetMapping("/usernameIsExist")
@@ -86,7 +91,7 @@ public class PassportController extends BaseController{
 
         // TODO 生成用户token，存入redis会话(分布式的会话)
         // TODO 同步购物车数据（也是到redis）
-
+        synCookieAndRedisData(userResult.getId(), request, response);
         return Response.ok();
     }
 
@@ -121,7 +126,7 @@ public class PassportController extends BaseController{
 
         // TODO 生成用户token，存入redis会话
         // TODO 同步购物车数据 （多台电脑之间的数据同步）
-
+        synCookieAndRedisData(userResult.getId(), request, response);
         return Response.ok(userResult);
     }
 
@@ -133,6 +138,56 @@ public class PassportController extends BaseController{
         userResult.setUpdatedTime(null);
         userResult.setBirthday(null);
         return userResult;
+    }
+
+    /**
+     * 同步cookie和redis的数据
+     *
+     * @param userId
+     * @param request
+     * @param response
+     */
+    private void synCookieAndRedisData(String userId, HttpServletRequest request, HttpServletResponse response){
+        String shopcatFromRedis = redisOperator.get(FOODIE_SHOPCART + ":" + userId);
+        String shopcatFromCookie = CookieUtils.getCookieValue(request, FOODIE_SHOPCART, true);
+        List<ShopCatBO> shopCatBOFromRedis = new ArrayList<>();
+        List<ShopCatBO> shopCatBOFromCookie = new ArrayList<>();
+        if (StringUtils.isNotBlank(shopcatFromRedis)) {
+            shopCatBOFromRedis = JSONObject.parseArray(shopcatFromRedis, ShopCatBO.class);
+        }
+        if (StringUtils.isNotBlank(shopcatFromCookie)) {
+            shopCatBOFromCookie = JSONObject.parseArray(shopcatFromCookie, ShopCatBO.class);
+        }
+        /**
+         * redis 中购物车数据为空，cookie也为空，不做处理
+         *                       cookie不为空，cookie放入redis
+         * redis 中购物车数据不为空，cookie为空，放入cookie
+         *                         cookie不为空，且cookie中商品在redis中存在的，以cookie为主，覆盖掉该商品
+         *                                      剩余不存在，直接加上
+         *redis覆盖cookie
+         */
+        if (CollectionUtils.isEmpty(shopCatBOFromRedis)) {
+            if (!CollectionUtils.isEmpty(shopCatBOFromCookie)) {
+                shopCatBOFromRedis.addAll(shopCatBOFromCookie);
+            }
+        } else {
+            List<ShopCatBO> pendingRemovedCat = new ArrayList<>();
+            List<ShopCatBO> pendingAddCat = new ArrayList<>();
+            if (!CollectionUtils.isEmpty(shopCatBOFromCookie)) {
+                for (ShopCatBO shopCatRedis: shopCatBOFromRedis) {
+                    for (ShopCatBO shopCatCookie : shopCatBOFromCookie) {
+                        if (StringUtils.equals(shopCatCookie.getSpecId(), shopCatRedis.getSpecId())) {
+                            pendingAddCat.add(shopCatCookie);
+                            pendingRemovedCat.add(shopCatRedis);
+                        }
+                    }
+                }
+                shopCatBOFromRedis.removeAll(pendingRemovedCat);
+                shopCatBOFromRedis.addAll(pendingAddCat);
+            }
+        }
+        redisOperator.set(FOODIE_SHOPCART + ":" + userId, JSONObject.toJSONString(shopCatBOFromRedis));
+        CookieUtils.setCookie(request, response, FOODIE_SHOPCART, JSONObject.toJSONString(shopCatBOFromRedis), true);
     }
 
 
