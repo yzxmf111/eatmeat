28be0e71a23dccad663ff2b8c8ed6c0cf8b9aec9
田一帆 <yifan.tian@tongdun.net>
2022-08-12 10:13:29 +0800

    redis分布式会话实现2---拦截器

--- eatmeat-api/src/main/java/com/xiaotian/config/UserLoginInterceptor.java
+++ eatmeat-api/src/main/java/com/xiaotian/config/UserLoginInterceptor.java
@@ -1,22 +1,34 @@
 package com.xiaotian.config;
 
-import com.xiaotian.utils.CookieUtils;
-import org.springframework.stereotype.Component;
+import com.alibaba.fastjson.JSONObject;
+import com.xiaotian.utils.RedisOperator;
+import com.xiaotian.utils.Response;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * @author xiaotian
  * @description
  * @date 2022-08-11 09:21
  */
-@Component
 public class UserLoginInterceptor implements HandlerInterceptor {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserLoginInterceptor.class);
+
+    private static final String USER_TOKEN = "user_token";
+
+    @Autowired
+    private RedisOperator redisOperator;
     /**
      * 访问controller之前
      * @param request
@@ -34,7 +46,47 @@ public class UserLoginInterceptor implements HandlerInterceptor {
         String userId = request.getHeader("headerUserId");
         String token = request.getHeader("headerUserToken");
 
+        if(StringUtils.isAnyBlank(userId, token)) {
+            LOGGER.error("userId = {}, token = {} 中存在空值", userId, token);
+            // * 拦截器处理只能返回true or false
+            //     * 使用这个方法可以将返回放在response中
+            returnErrorResponse(response, Response.errorMsg("请登录"));
+            return false;
+        }
+        String tokenFromRedis = redisOperator.get(USER_TOKEN + ":" + userId);
+        if (!StringUtils.equals(tokenFromRedis, token)) {
+            LOGGER.error("tokenFromRedis={}, token={}", tokenFromRedis, token);
+            returnErrorResponse(response, Response.errorMsg("请登录"));
+            return false;
+        }
         return true;
+    }
+
+    /**
+     * 拦截器处理只能返回true or false
+     * 使用这个方法可以将返回放在response中
+     * @param response
+     * @param res
+     */
+    private void returnErrorResponse(HttpServletResponse response, Response res) {
+        OutputStream os = null;
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("application/json");
+        try {
+            os = response.getOutputStream();
+            os.write(JSONObject.toJSONString(res).getBytes("utf-8"));
+            os.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (os != null) {
+                    os.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
--- eatmeat-api/src/main/java/com/xiaotian/config/WebMvcConfig.java
+++ eatmeat-api/src/main/java/com/xiaotian/config/WebMvcConfig.java
@@ -1,6 +1,8 @@
 package com.xiaotian.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,4 +26,36 @@ public class WebMvcConfig implements WebMvcConfigurer {
     }
 
 
+    /**
+     * 1.拦截器加入容器
+     */
+
+    @Bean
+    public UserLoginInterceptor getUserLoginInterceptor() {
+        return new UserLoginInterceptor();
+    }
+    /**
+     * 2.拦截器注册
+     * 3.拦截器注册器加入WebMvcConfigurer
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(getUserLoginInterceptor())
+                .addPathPatterns("/hello")
+                .addPathPatterns("/shopcart/add")
+                .addPathPatterns("/shopcart/del")
+                .addPathPatterns("/address/list")
+                .addPathPatterns("/address/add")
+                .addPathPatterns("/address/update")
+                .addPathPatterns("/address/setDefalut")
+                .addPathPatterns("/address/delete")
+                .addPathPatterns("/orders/*")
+                .addPathPatterns("/center/*")
+                .addPathPatterns("/userInfo/*")
+                .addPathPatterns("/myorders/*")
+                .addPathPatterns("/mycomments/*")
+                .excludePathPatterns("/myorders/deliver")
+                .excludePathPatterns("/orders/notifyMerchantOrderPaid");
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
 }
